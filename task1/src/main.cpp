8e2e5b3a12a85970b60d1e330e1f8fe26f40449f
mmmhdp <malcevnik99@gmail.com>
2026-02-12 11:49:37 +0300

    feat: defines an interfaces structure
    In the context of the task we assume an
    existance of a low lvl driver for an chip.
    
    I emulate this one with pure virtual interface
    for that driver which have some really basic
    io capabilities (gpio - general purpose io).
    In reality it should be look like registers
    manipulation to my understanding.
    
    Then I have a SPI protocol wrapper to emulate
    communication pattern according to a
    description of the protocol itself.
    But here also defined a transfer function
    that not match the protocol itself since
    the SPI transfers by BIT, but EEPROM
    defines api by BYTE, so it's kind of an
    extention for a conveniens.
    
    And finally, EEPROM class with higher
    level api that tasks expects from implementation.
    Basically SPI primitives going to be abused here.
    
    NOTE - final implementation could be not same as
    defined for now. At least some mocks are
    needed in the favor of debugging.

--- task1/src/main.cpp
+++ task1/src/main.cpp
@@ -1,3 +1,97 @@
+#include <cstdint>
+
+struct i_chip_gpio_driver {
+  virtual void setHigh(int pin) = 0;
+  virtual void setLow(int pin) = 0;
+  virtual bool read(int pin) = 0;
+  virtual ~i_chip_gpio_driver() = default;
+};
+
+class chip_spi_api {
+public:
+  chip_spi_api(i_chip_gpio_driver &gpio, int pinCS, int pinSCK, int pinMOSI,
+               int pinMISO);
+  /*
+   * To begin communication
+   * master have to
+   * set CS to LOW on the slave device
+   *
+   * on contrary, deselect ends
+   * communication and sets
+   * slave device's CS to HIGH
+   * */
+
+  void select();
+  void deselect();
+
+  /*
+   * SPI assume data transfers by bits,
+   * but we need byte transfer interface
+   * inplace since EEPROM device is
+   * capable only of understand
+   * by byte and not by bit communication scenario
+   * */
+  uint8_t transfer(uint8_t data);
+
+private:
+  /*
+   * SPI primitives
+   * */
+
+  void clockPulse();
+  void writeBit(bool bit);
+  bool readBit();
+
+private:
+  i_chip_gpio_driver &m_gpio;
+
+  int pinCS_;
+  int pinSCK_;
+  int pinMOSI_;
+  int pinMISO_;
+};
+
+class eeprom_api {
+public:
+  explicit eeprom_api(chip_spi_api &spi);
+
+  /*
+   * eeprom api we want to have
+   * */
+
+  void writeByte(uint16_t address, uint8_t data);
+  uint8_t readByte(uint16_t address);
+
+  void writeBuffer(uint16_t address, const uint8_t *data, std::size_t length);
+  void readBuffer(uint16_t address, uint8_t *buffer, std::size_t length);
+
+  void writeBit(uint16_t address, uint8_t bitPosition, bool value);
+  bool readBit(uint16_t address, uint8_t bitPosition);
+
+private:
+  /*
+   * eeprom primitives
+   * */
+  void writeEnable();
+  uint8_t readStatus();
+  void waitUntilReady();
+
+  void sendCommand(uint8_t command);
+  void sendAddress(uint16_t address);
+
+private:
+  chip_spi_api &spi_api_;
+
+  /*
+   * eeprom commands
+   * */
+  static constexpr uint8_t CMD_READ = 0x03;
+  static constexpr uint8_t CMD_WRITE = 0x02;
+  static constexpr uint8_t CMD_WREN = 0x06;
+  static constexpr uint8_t CMD_RDSR = 0x05;
+  static constexpr uint8_t CMD_WRSR = 0x05;
+};
+
 int main (void) {
   return 0;
 }
